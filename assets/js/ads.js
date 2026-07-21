8cf15ffbfff585ec1149dab15deffaa036f0e870
Mireli5656 <miraliseyidli5656@gmail.com>
2026-07-21 12:29:30 +0400

    Update ads.js

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,22 +1,22 @@
-const adTests = [
+[
   {
-    name: "Google Ads",
-    url: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    "name": "Google Ads",
+    "url": "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
   },
   {
-    name: "Google Syndication",
-    url: "https://pagead2.googlesyndication.com/pagead/js/show_ads.js"
+    "name": "Google Syndication",
+    "url": "https://pagead2.googlesyndication.com/pagead/js/show_ads.js"
   },
   {
-    name: "DoubleClick",
-    url: "https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+    "name": "DoubleClick",
+    "url": "https://securepubads.g.doubleclick.net/tag/js/gpt.js"
   },
   {
-    name: "Google Tag Services",
-    url: "https://www.googletagservices.com/tag/js/gpt.js"
+    "name": "Google Tag Services",
+    "url": "https://www.googletagservices.com/tag/js/gpt.js"
   },
   {
-    name: "Media.net",
-    url: "https://contextual.media.net/dmedianet.js"
+    "name": "Media.net",
+    "url": "https://contextual.media.net/dmedianet.js"
   }
-];
+]
