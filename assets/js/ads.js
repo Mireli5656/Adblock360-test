2bd7b68dda2380cd176529e37fedd6b4bc16e97d
Mireli5656 <miraliseyidli5656@gmail.com>
2026-07-21 12:01:41 +0400

    Create ads.js

---
+++ assets/js/ads.js
@@ -0,0 +1,10 @@
+const adTests = [
+  {
+    name: "Google Ads",
+    url: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+  },
+  {
+    name: "DoubleClick",
+    url: "https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+  }
+];
